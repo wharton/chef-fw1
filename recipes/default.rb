bf77bc54336d53395ffbccdff6cd5aacd5ad7107
Nathan Mische <nmische@gmail.com>
2013-02-14 10:44:06 -0500

    Updating default owner.

--- recipes/default.rb
+++ recipes/default.rb
@@ -34,6 +34,8 @@ when "1"
 end
  
 file_name = zip_url.split('/').last
+
+node.set['fw1']['owner'] = node['cf10']['installer']['runtimeuser'] if node['fw1']['owner'] == nil
  
 # Download FW1
  
@@ -49,8 +51,8 @@ end
 # Create the target install directory if it doesn't exist
  
 directory "#{node['fw1']['install_path']}/org/corfield" do
-  owner "root"
-  group "root"
+  owner node['fw1']['owner']
+  group node['fw1']['group']
   mode "0755"
   action :create
   recursive true
@@ -65,7 +67,7 @@ script "install_fw1" do
   cwd "#{Chef::Config['file_cache_path']}"
   code <<-EOH
 unzip -j #{file_name} **framework.cfc -d #{node['fw1']['install_path']}/org/corfield
-chown -R nobody:bin #{node['fw1']['install_path']}
+chown -R #{node['fw1']['owner']}:#{node['fw1']['group']} #{node['fw1']['install_path']}
 EOH
   not_if { File.exists?("#{node['fw1']['install_path']}/org/corfield/framework.cfc") }
 end
